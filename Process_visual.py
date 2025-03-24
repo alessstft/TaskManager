ad6661ec24cb75f8065054d776b36a4c0a1e678d
alessstft <alesssftf@gmail.com>
2025-03-24 14:12:14 +0300

    Update Process_visual.py

--- Process_visual.py
+++ Process_visual.py
@@ -1,31 +1,33 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
 
 root = tk.Tk()
-root.title("Task Manager")
+root.title("Диспетчер задач")
+icon = PhotoImage(file = "icon.png")
+root.iconphoto(False, icon)
 root.geometry("600x400")
-root.configure(bg="#2d2d2d")
+root.configure(bg="#808000")
 
-
-root.attributes('-alpha', 0.95) 
+root.attributes('-alpha', 0.94) 
 
 # Стили
 style = ttk.Style()
 style.theme_use("clam")
-style.configure("TNotebook", background="#2d2d2d", borderwidth=0)
-style.configure("TNotebook.Tab", background="#5c2d5c", foreground="white", padding=[10, 5])
-style.map("TNotebook.Tab", background=[("selected", "#872187")])
+style.configure("TNotebook", background="#696969", borderwidth=0)
+style.configure("TNotebook.Tab", background="#696969", foreground="white", padding=[10, 5])
+style.map("TNotebook.Tab", background=[("selected", "#000000")])
 
-style.configure("Treeview", background="#872187", foreground="white", fieldbackground="#872187")
-style.configure("Treeview.Heading", background="#5c2d5c", foreground="white")
+style.configure("Treeview", background="#000000", foreground="white", fieldbackground="#000000")
+style.configure("Treeview.Heading", background="#000000", foreground="white")
 
 notebook = ttk.Notebook(root)
 notebook.pack(fill=tk.BOTH, expand=True)
 
 # Вкладки
-frame1 = tk.Frame(notebook, bg="#872187")
-frame2 = tk.Frame(notebook, bg="#2d2d2d")
-frame3 = tk.Frame(notebook, bg="#2d2d2d")
+frame1 = tk.Frame(notebook, bg="#808080")
+frame2 = tk.Frame(notebook, bg="#808080")
+frame3 = tk.Frame(notebook, bg="#808080")
 notebook.add(frame1, text="Процессы")
 notebook.add(frame2, text="Производительность")
 notebook.add(frame3, text="Службы")
